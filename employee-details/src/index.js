af2a36ea0153cc7133200ee9e2c95b8b6a00eb78
Areis <62277220+Areis1004@users.noreply.github.com>
2020-04-14 17:01:00 +0530

    Add files via upload

---
+++ employee-details/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import EmployeeList from "./components/EmployeeList";
+
+ReactDOM.render(<EmployeeList></EmployeeList>, document.getElementById("root"))
+
+
+
+
+
+
